8c61e9ec136922caa832ed38c2440679accd3108
degree2466 <uzochukwugodwin959@gmail.com>
2023-03-29 04:52:23 +0200

    advanced tasks

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
+
+import math
+
+
+class MagicClass:
+    """Represent a circle."""
+
+    def __init__(self, radius=0):
+        """Initialize a MagicClass.
+        Arg:
+            radius (float or int): The radius of the new MagicClass.
+        """
+        self.__radius = 0
