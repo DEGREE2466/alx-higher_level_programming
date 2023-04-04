b46ccae6ec473df5ed94443daabc7b8b83386baa
degree2466 <uzochukwugodwin959@gmail.com>
2023-04-04 01:00:12 -0400

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -50,7 +50,8 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
+    
+    
     def area(self):
         """
             Calculates the area of a rectangle
