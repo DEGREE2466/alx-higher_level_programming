4971dfa42753417d0f53dc14f9350fbb6fa8d20e
degree2466 <uzochukwugodwin959@gmail.com>
2023-07-02 18:55:03 -0700

    made executable

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL
-- sends a POST request to the passed URL
+"""This is a script that:
+- takes in the URL
+- sends a POST request to the URL passed
 - takes email as a parameter
 - displays the body of the response
 """
